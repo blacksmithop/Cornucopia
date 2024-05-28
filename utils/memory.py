399f459896eab4beead11c55b73b1839a22f70c8
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-05-28 12:18:22 +0530

    feat: Memory with session id

---
+++ utils/memory.py
@@ -0,0 +1,10 @@
+from langchain.memory import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+
+
+message_store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in message_store:
+        message_store[session_id] = ChatMessageHistory()
+    return message_store[session_id]
