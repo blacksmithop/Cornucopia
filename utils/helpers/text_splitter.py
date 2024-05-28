f1485f63f1159aa16da9a8809373e3a48f3d737d
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-05-28 12:17:35 +0530

    feat: Helper scripts

---
+++ utils/helpers/text_splitter.py
@@ -0,0 +1,13 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=20,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+def get_relevant_chunks(text: str):
+    docs = text_splitter.create_documents([text])
+    text_chunks = [doc.page_content for doc in docs[:2]]
+    return " ".join(text_chunks)
