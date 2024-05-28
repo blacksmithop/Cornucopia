9b3a6cbf5325007cefeb68aaa6fb7214e5cf0770
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-05-28 12:17:01 +0530

    feat: Retrievers, embed reorder retrienver

---
+++ utils/retrievers.py
@@ -0,0 +1,29 @@
+from langchain_community.retrievers.wikipedia import WikipediaRetriever
+from langchain_chroma import Chroma
+import os, chromadb
+from utils.embeddings import instruct_embeddings
+
+# wikipedia
+wiki_retriever = WikipediaRetriever()
+
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = os.path.join(ABS_PATH, "chroma_db")
+
+
+client_settings = chromadb.config.Settings(
+    is_persistent=True,
+    persist_directory=DB_DIR,
+    anonymized_telemetry=False,
+)
+
+# chromadb
+upload_docs_db = Chroma(
+    collection_name="project_store_all",
+    persist_directory=DB_DIR,
+    client_settings=client_settings,
+    embedding_function=instruct_embeddings,
+)
+
+custom_retriever = upload_docs_db.as_retriever(
+    search_type="similarity", search_kwargs={"k": 5, } # "include_metadata": True
+)
