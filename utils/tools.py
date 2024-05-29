16272cb72b5a583cf465251c75242f0d23003125
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-05-29 18:22:20 +0530

    feat: Add small talk agent

--- utils/tools.py
+++ utils/tools.py
@@ -17,10 +17,14 @@ wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
 class SearchInput(BaseModel):
     query: str = Field(description="Used for search query that ask for metadata, detailed information, references etc.")
+    
+class ConversationInput(BaseModel):
+    query: str = Field(description="Used for handling small talk and similar conversation. Use when no suitable action is found. Ask for clarification if necessary")
+    
 
 class CustomKnowledgeBaseTool(BaseTool):
-    name = "custom_search"
-    description = "Used for search query that ask for metadata, detailed information, references etc."
+    name = "small_talk"
+    description = "Used for handling small talk and similar conversation. Use when no suitable action is found. Ask for clarification if necessary"
     args_schema: Type[BaseModel] = SearchInput
     return_direct = True
     def _run(
@@ -36,6 +40,25 @@ class CustomKnowledgeBaseTool(BaseTool):
     ) -> str:
         """Use the tool asynchronously."""
         raise NotImplementedError("custom_search does not support async")
+
+class CustomKnowledgeBaseTool(BaseTool):
+    name = "small_talk"
+    description = "Used for search query that ask for metadata, detailed information, references etc."
+    args_schema: Type[BaseModel] = SearchInput
+    return_direct = True
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        response = supplemented_knowledge_base.get_relevant_documents(query=query)
+        parsed_response = parse_retriever_content(response=response)
+        return parsed_response
+    
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("small_talk does not support async")
 
 
 tool_list = [search, wikipedia, CustomKnowledgeBaseTool()]
